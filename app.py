2ca0d5d9bafe681b04e36d418bbe9baeed3c5dd6
thedon45 <92436587+thedon45@users.noreply.github.com>
2022-05-17 10:33:24 +0300

    copying source code from another foreign place

--- app.py
+++ app.py
@@ -30,11 +30,20 @@ def products():
     except Exception as e:
         print(e)
     finally:
-        rows = cursor.fetchall()
-        cartPage = render_template('products.html', products=rows)
-        return cartPage
+        pageProducts = cursor.fetchall()
+        return render_template('products.html', products=pageProducts)
         cursor.close()
         conn.close()
+
+def array_merge(first_array,second_array):
+    if isinstance(first_array,list) and isinstance( second_array,list):
+        return first_array + second_array
+    elif isinstance(first_array , dict) and isinstance(second_array , dict):
+        return dict(list(first_array.items() ) + list(second_array.items() ) )
+    elif isinstance( first_array,set) and isinstance(second_array,set):
+        return first_array.union(second_array)
+    return False
+
 
 @app.route('/empty')
 def empty_cart():
@@ -70,21 +79,7 @@ def delete_product(code):
         print(e)
 
 
-
-
-
-def array_merge(first_array,second_array):
-    if isinstance(first_array,list) and isinstance( second_array,list):
-        return first_array + second_array
-    elif isinstance(first_array,dict) and isinstance(second_array,dict):
-        return dict(list(first_array.items() ) + list(second_array.items() ) )
-    elif isinstance( first_array,set) and isinstance(second_array,set):
-        return first_array.union(second_array)
-    return False
-
-
-
-@app.route('/add',methods=['POST','GET'])
+@app.route('/add',methods=['POST'])
 def add_product_to_cart():
     cursor=None
     try:
@@ -92,51 +87,54 @@ def add_product_to_cart():
         _code=request.form['code']
         #validate the received values
         if _quantity and _code and request.method =='POST':
-            conn=mysql.connect()
-            cursor=conn.cursor(pymysql.cursors.DictCursor)
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
             cursor.execute('SELECT * FROM product WHERE code=%s', _code)
             row=cursor.fetchone()
-            itemArray={row['code']:{'name':row['name'],'code':row['code'],'quantity':_quantity,'price':row['price'],'image' : row['image'],'total_price':_quantity *row['price']}}
-            all_total_price =0
-            all_total_quantity=0
+
+            itemArray={ row['code'] : {'name' :row['name'],'code':row['code'],'quantity': _quantity, 'price' :row['price'],'image' :row['image'],'total_price':_quantity * row['price']}}
+
+            all_total_price = 0
+            all_total_quantity = 0
 
             session.modified=True
             if 'cart_item' in session:
                 if row['code'] in session['cart_item']:
-                    for key,value in session['cart_item'].items():
-                        if row['code']==key:
-                            old_quantity=session['cart_item'][key]['quantity']
-                            total_quantity=old_quantity +_quantity
+                    for key, value in session['cart_item'].items():
+                        if row['code'] == key:
+                            old_quantity = session['cart_item'][key]['quantity']
+                            total_quantity=old_quantity + _quantity
                             session['cart_item'][key]['quantity']=total_quantity
-                            session['cart_item'][key]['total_price']=total_quantity *row[price]
+                            session['cart_item'][key]['total_price']=total_quantity * row['price']
                 else:
-                    session['cart_item'] = array_merge(session['cart_item'],itemArray)
+                    session['cart_item'] = array_merge(session['cart_item'], itemArray)
 
 
                 for key,value in session['cart_item'].items():
-                    individual_quantity=int(session['cart_item'][key]['quantity'])
-                    individual_price=float(session['cart_item'][key]['total_price'])
-                    all_total_quantity=all_total_quantity + individual_quantity
-                    all_total_price=all_total_price + individual_price
+                    individual_quantity = int(session['cart_item'][key]['quantity'])
+                    individual_price = float(session['cart_item'][key]['total_price'])
+                    all_total_quantity = all_total_quantity + individual_quantity
+                    all_total_price = all_total_price + individual_price
 
             else:
-                session['cart_item']=itemArray
-                all_total_quantity=all_total_quantity +_quantity
+                session['cart_item'] = itemArray
+                all_total_quantity=all_total_quantity + _quantity
                 all_total_price=all_total_price + _quantity * row['price']
             session['all_total_quantity']= all_total_quantity
             session['all_total_price']=all_total_price
 
+            return redirect(url_for('.products'))
+
 
         else:
-            return 'EROR While Adding items to the cart'
+            return 'ERROR While Adding items to the cart'
 
     except Exception as e:
         print(e)
     finally:
-        output = redirect(url_for('products'))
-        return output
+        return redirect(url_for('products'))
         cursor.close()
-        conn.close
+        conn.close()
 
 
 
